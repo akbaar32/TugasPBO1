422d50d50a20c5b4151f0221df84d3ed183ea124
akbaar32 <muhammad.akbaar32@gmail.com>
2018-03-28 00:09:10 +0800

    mengisi nilai Atribut

--- src/tugaspbo1/KaryawanAksi.java
+++ src/tugaspbo1/KaryawanAksi.java
@@ -9,5 +9,29 @@ public class KaryawanAksi {
         Karyawan Awang = new Karyawan();
         Karyawan Iqbal = new Karyawan();
         Karyawan Ebay  = new Karyawan();
+        
+        Endra.NIK="16.63.0487";
+        Endra.Nama="Maulana Endrawan";
+        Endra.Jabatan="Direktur";
+        Endra.LamaBekerja="5 Tahun";
+        Endra.Gaji=" Rp.200.000.000";
+        
+        Awang.NIK="16.63.0023";
+        Awang.Nama="Awang Parbasik Bahwana";
+        Awang.Jabatan="Wakil Direktur";
+        Awang.LamaBekerja="3 Tahun";
+        Awang.Gaji=" Rp.50.000.000";
+        
+        Iqbal.NIK="16.63.0488";
+        Iqbal.Nama="Muhammad Iqbal";
+        Iqbal.Jabatan="Staff";
+        Iqbal.LamaBekerja="1 Tahun";
+        Iqbal.Gaji=" Rp.35.000.000";
+        
+        Ebay.NIK="16.63.5589";
+        Ebay.Nama="Ebay";
+        Ebay.Jabatan="Staff";
+        Ebay.LamaBekerja="1 Tahun";
+        Ebay.Gaji=" Rp.35.000.000";
     }
 }
