aefdf828ccba0e62e484f3e6aabe51f41939f49c
akbaar32 <muhammad.akbaar32@gmail.com>
2018-03-27 23:52:26 +0800

    Membuat Atribut dan Behavior

--- src/tugaspbo1/Karyawan.java
+++ src/tugaspbo1/Karyawan.java
@@ -3,5 +3,18 @@ package tugaspbo1;
 
 
 public class Karyawan {
+    String NIK;
+    String Nama;
+    String Jabatan;
+    String LamaBekerja;
+    String Gaji;
     
+    void cetakinfo (){
+        System.out.println("NIK Karyawan        :"+NIK);
+        System.out.println("Nama Karyawan       :"+Nama);
+        System.out.println("Jabat Karyawan      :"+Jabatan);
+        System.out.println("Lama Bekerja        :"+LamaBekerja);
+        System.out.println("Gaji Perbulan       :"+Gaji);
+        System.out.println("**********************************");
+    }
 }
