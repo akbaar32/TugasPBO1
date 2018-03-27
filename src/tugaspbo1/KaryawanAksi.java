9ac961660aa47c61d1fd3bd10c41e92af0d9d5c4
akbaar32 <muhammad.akbaar32@gmail.com>
2018-03-27 23:59:03 +0800

    Instance Objek

--- src/tugaspbo1/KaryawanAksi.java
+++ src/tugaspbo1/KaryawanAksi.java
@@ -5,5 +5,9 @@ package tugaspbo1;
 public class KaryawanAksi {
     public static void main(String[] args) {
         
+        Karyawan Endra = new Karyawan();
+        Karyawan Awang = new Karyawan();
+        Karyawan Iqbal = new Karyawan();
+        Karyawan Ebay  = new Karyawan();
     }
 }
